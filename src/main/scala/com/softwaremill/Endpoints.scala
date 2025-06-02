42ba63efaa61c5d2d449e19b424b3d7d2149dfd2
Yasmine <Yasmine>
2025-06-02 15:13:23 +0300

    add getdata  Api

--- src/main/scala/com/softwaremill/Endpoints.scala
+++ src/main/scala/com/softwaremill/Endpoints.scala
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b9d9528e3edd8615fa57a5d1901821601e564715b1a151197f64361d7bf5dc4
-size 2751
+oid sha256:c39d18fdf82c6dec4a2e3700e440bba2ad6207ee8cf3d853e9939a1246567d84
+size 2980
